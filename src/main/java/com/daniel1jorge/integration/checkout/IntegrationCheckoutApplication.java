f94496bc802c64958e271af9b0597788921aeb31
Jorge Daniel Silva <jorge1daniel@gmail.com>
2024-01-05 16:52:15 -0300

    Primer commit >>>

---
+++ src/main/java/com/daniel1jorge/integration/checkout/IntegrationCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.daniel1jorge.integration.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationCheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel1jorge/integration/checkout/IntegrationCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel1jorge.integration.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
